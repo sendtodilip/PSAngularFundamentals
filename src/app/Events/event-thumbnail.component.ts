f63508d09c7916d145fadd577d5c44f6bcbdb465
sendtodilip <sendtodilip>
2019-12-04 12:28:28 +0530

    Exploring the Angular template Syntax

--- src/app/Events/event-thumbnail.component.ts
+++ src/app/Events/event-thumbnail.component.ts
@@ -5,17 +5,30 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     template: `
         <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{ event.price }}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <div>Date: {{event?.date}}</div>
+        <!--[class.green]="event?.time==='8:00 am'"
+        [ngClass]="{green: event?.time ==='8:00 am', bold: event?.time==='8:00 am'}"
+        [ngClass]="getStartTimeClass()"
+        [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 
+        'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}"  -->
+        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
         </div>
-        <button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button>
+        <div>Price: \${{ event?.price }}</div>
+        <!--<div *ngIf="event?.location">-->
+        <div [hidden]="!event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
         </div>
     `,
     styles: [`
+        .green {color: green !important;}
+        .bold {font-weight: bold;}
+        .thumbnail {min-height: 210px;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb;}
     `]
@@ -34,5 +47,24 @@ export class EventThumbnailComponent {
 
     logFoo() {
         console.log('Log Foo');
+    }
+
+    getStartTimeClass() {
+        if (this.event && this.event.time === '8:00 am') {
+            return 'green bold';
+        } else {
+            return '';
+
+        }
+        // const isEearlyStart = 
+        // return {green:isEearlyStart, bold: isEearlyStart}
+    }
+
+    getStartTimeStyle() {
+        if (this.event && this.event.time === '8:00 am') {
+            return {color: '#003300', 'font-weight': 'bold'};
+        } else {
+            return [];
+        }
     }
 }
